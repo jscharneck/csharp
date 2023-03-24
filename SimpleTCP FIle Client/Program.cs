274b5efc5142cc6f3e023688b288f3517f2468cd
Jose Scharneck <jose.scharneck87@gmail.com>
2023-03-24 11:42:38 +0200

    a

---
+++ SimpleTCP FIle Client/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public class Program
+{
+
+    public static void Main(String[] args)
+    {
+        List<string> filesList = new List<string>();
+
+        string path = @"files/";
+
+        string[] fileEntries = Directory.GetFiles(path);
+
+        foreach (string fileName in fileEntries)
+        {
+            filesList.Add(Path.GetFileName(fileName));
+        }
+
+        // string file01 = @"boo_and_baba.jpg";
+        // string file02 = @"dalio01.jpg";
+
+        //convert string to byte array to pass in a name of file
+
+        // Console.WriteLine("Dns.GetHostAddresses :" + Dns.GetHostAddresses(Dns.GetHostName())[0]);
+        // Console.WriteLine("Dns.GetHostAddresses :" + Dns.GetHostAddresses(Dns.GetHostName())[1]);
+        // Console.WriteLine("Dns.GetHostAddresses :" + Dns.GetHostAddresses(Dns.GetHostName())[2]);
+        Console.WriteLine("Dns.GetHostAddresses :" + Dns.GetHostAddresses(Dns.GetHostName())[3]);
+        // Console.WriteLine("Dns.GetHostAddresses :" + Dns.GetHostAddresses(Dns.GetHostName())[4]);
+
+
+
+        foreach (var file in filesList)
+        {
+            TcpClient clientSocket = new TcpClient(Dns.GetHostAddresses(Dns.GetHostName())[3].ToString(), 9000);
+
+            Stream fileStream = File.OpenRead(path + file);
+            // Console.WriteLine("My FileStream: " + fileStream.Length);
+
+            byte[] fileBuffer = new byte[fileStream.Length];
+
+            fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
+            // Stream.Read() reads/passes data into fileBuffer
+            // An array of bytes. 
+            // When this method returns, the buffer contains the specified byte array with the values...
+            // between offset and (offset + count - 1) replaced by the bytes read from the current source.
+
+            // Console.WriteLine("IPAddress.Loopback: " + IPAddress.Loopback);
+            // Console.WriteLine("IPAddress.Broadcast: " + IPAddress.Broadcast);
+            // Console.WriteLine("IPAddress.None: " + IPAddress.None);
+            // Console.WriteLine("IPAddress.Any: " + IPAddress.Any);
+
+
+            NetworkStream networkStream = clientSocket.GetStream();
+            networkStream.Write(fileBuffer, 0, fileBuffer.Length);
+            networkStream.Close();
+
+
+        }
+
+        // networkStream.Write(fileBuffer, 0, fileBuffer.GetLength(0));
+
+
+
+
+    }
+
+}
---
+++ SimpleTCP File Server/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+public class Program
+{
+    // private ArrayList alSockets;
+    private List<Socket> alSockets;
+
+    public Program()
+    {
+        // alSockets = new ArrayList();
+        alSockets = new List<Socket>();
+
+
+        IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+        // lblStatus.Text = "My IP address is " +
+        IPHost.AddressList[0].ToString();
+        Thread thdListener = new Thread(new
+        ThreadStart(listenerThread));
+        thdListener.Start();
+    }
+
+    public void listenerThread()
+    {
+        // TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 9000);
+        TcpListener tcpListener = new TcpListener(Dns.GetHostAddresses(Dns.GetHostName())[3], 9000);
+        tcpListener.Start();
+        while (true)
+        {
+            Console.WriteLine("Waiting for connection...");
+            Socket handlerSocket = tcpListener.AcceptSocket();
+            if (handlerSocket.Connected)
+            {
+
+                Console.WriteLine(" connected.");
+                lock (this)
+                {
+                    alSockets.Add(handlerSocket);
+                }
+                ThreadStart thdstHandler = new ThreadStart(handlerThread);
+                Thread thdHandler = new Thread(thdstHandler);
+                thdHandler.Start();
+            }
+        }
+    }
+
+    public void handlerThread()
+    {
+        Socket handlerSocket = (Socket)alSockets[alSockets.Count - 1];
+        NetworkStream networkStream = new NetworkStream(handlerSocket);
+        int thisRead = 0;
+        int blockSize = 1024;
+        Byte[] dataByte = new Byte[blockSize];
+        lock (this)
+        {
+            // Only one process can access
+            // the same file at any given time
+            // Stream fileStream = File.OpenWrite(@"dalio01_received" + DateTime.Now.Millisecond + ".jpg");
+            Stream fileStream = File.OpenWrite(@"receivedfiles\file_" + DateTime.Now.Millisecond + ".jpg");
+
+            while (true)
+            {
+                thisRead = networkStream.Read(dataByte, 0, blockSize);
+                fileStream.Write(dataByte, 0, thisRead);
+                if (thisRead == 0) break;
+            }
+            fileStream.Close();
+        }
+        Console.WriteLine("File Written");
+        handlerSocket = null;
+    }
+
+
+    public static void Main(String[] args)
+    {
+
+        new Program();
+    }
+
+}
